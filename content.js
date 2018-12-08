9fc77fba8cd0429b24eeab207038684ae8e1c4b2
Nick Doty <nickdoty@nicks-mbp.lan>
2018-12-08 07:36:00 -0500

    Working version 0.1

--- content.js
+++ content.js
@@ -5,10 +5,12 @@ for (var i = 0; i < elements.length; i++) {
 
     for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
+        var wordsToReplace = /Donald Trump|Donald J Trump|Donald J. Trump|President Trump|Donald John Trump/ig;
+        var replacement = 'Individual-1';
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/[word or phrase to replace here]/gi, '[new word or phrase]');
+            var replacedText = text.replace(wordsToReplace, replacement);
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
